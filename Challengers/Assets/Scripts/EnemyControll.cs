fab881fd7b87c472f79c262b2cd7c1e630988376
KDonghoo <dh1996m@naver.com>
2020-09-12 21:07:06 +0900

    [동호] 코드수정 조금

--- Challengers/Assets/Scripts/EnemyControll.cs
+++ Challengers/Assets/Scripts/EnemyControll.cs
@@ -2,16 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyControll : MonoBehaviour
+public class EnemyControll : EnemyMeleeFSM
 {
     public GameObject enemyCanvasGo;
+   
+    private void Start()
+    {
+       
 
+    }
+
+    private void Update()
+    {
+
+        
+       
+        if (currentHp <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.CompareTag ("Player"))
         {
             enemyCanvasGo.GetComponent<EnemyHpBar>().currentHp -= 300f;
+            currentHp -= 300f;
+        }
+
+       else if (collision.transform.CompareTag("wall")) 
+        {
+           
         }
     }
 }
