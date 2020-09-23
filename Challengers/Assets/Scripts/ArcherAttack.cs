4e505be006ac07873870d784e14bae316819f5f9
DESKTOP-KQ7QRCQ\24565 <2456542@naver.com>
2020-09-23 17:33:32 +0900

    궁수 만들기 시작

---
+++ Challengers/Assets/Scripts/ArcherAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherAttack : MonoBehaviour
+{
+    private PlayerCtrl pc;
+    private Animator anim;
+    public Transform spawnPoint;
+
+    private bool isAttack;
+
+    public GameObject arrow;
+
+    private void Start()
+    {
+        pc = GameObject.FindWithTag("Player").GetComponent<PlayerCtrl>();
+        anim = GetComponent<Animator>();
+        isAttack = false;
+    }
+
+    private void Update()
+    {
+        anim.SetBool("isAttack", isAttack);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        pc.canMove = false;
+        anim.Play("ArcherAtk");       
+    }
+
+    private void ShootArrow()
+    {
+        Instantiate(arrow, spawnPoint);
+    }
+
+    public void EndAttack()
+    {
+        pc.canMove = true;
+    }
+}
